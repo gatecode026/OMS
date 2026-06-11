08dffdf5602e02e36fe477e0c039179959c85a62
RATIWALRAHULKUMAWAT <157282152+RATIWALRAHUL@users.noreply.github.com>
2026-06-11 14:59:05 +0530

    feat: implement attendance repository for CRUD operations and query filtering

--- backend/src/modules/attendance/attendance.repository.js
+++ backend/src/modules/attendance/attendance.repository.js
@@ -12,9 +12,9 @@ export const find = async (query = {}) => {
   if (query.employeeId) filters.employeeId = query.employeeId;
   if (query.from && query.to) {
     filters.date = { $gte: query.from, $lte: query.to };
-  } else if (query.date) {
+  } else if (query.date) {  
     filters.date = query.date;
-  }
+  }  
   if (query.branch) filters.branch = query.branch;
   if (query.department) filters.department = query.department;
   if (query.status) filters.status = query.status;
